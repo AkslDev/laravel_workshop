6231d8408f033cbbe7489bc44015fe15718351b0
Axel Coquin <aksl@macbook-pro-de-axel.home>
2018-08-26 19:26:54 +0200

    Affichage des données

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -1,10 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Page d'accueil</title>
-</head>
-<body>
+@extends('layout.master')
+
+@section('content')
+<div class="left-content">
+	@foreach ($posts as $posts)
+	<div class="item">
+		<a class="title" href="post/{{$posts->id}}">{{ $posts->titre }}</a>
+		<p class="type">{{ $posts->post_type }}</p>
+		<p class="description">{{ $posts->description }}</p>
+	</div>
+	@endforeach
+</div>
+<div class="right-content">	
     
-</body>
-</html>
+</div>
+@endsection
